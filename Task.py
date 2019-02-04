639ec5f524fbf6ba67e5efda4dd3cd9fbae655fc
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-04 15:46:42 +0300

    Basic class realization structure

--- Task.py
+++ Task.py
@@ -1,4 +1,15 @@
 
 
-class Color():
-    pass
+class Color:
+    def __init__(self, u_inp):
+        self.u_inp = u_inp
+
+    def __enter__(self):
+
+        self.u_inp = ('\033[47m''\033[31m'"{}".format(self.u_inp))
+        return self
+    
+    def __exit__(self, exc_type, exc_value, tb):
+        if self.u_inp:
+            self.u_inp = ("{}"'\033[00m''\033[00m'.format(self.u_inp))
+            return self
--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from Task import Color
 
 
 def u_input():
-    pass
+    print("here you could enter something")
+    user_input = input("Enter something: ")
+    with Color(user_input):
+        print(user_input)
