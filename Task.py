8fef108d6eee0ca332bba8588830f595fd4754fa
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-06 15:21:17 +0300

    Added new colors and fixed variables

--- Task.py
+++ Task.py
@@ -1,9 +1,22 @@
-class Color: 
+class Color:
+    BLACK = '30'
+    BLACK_b = '40' 
     RED = '31'
+    RED_b = '41'
     GREEN = '32'
+    GREEN_b = '42'
     YELLOW = '33'
-    WHITE_b = '47'
+    YELLOW_b = '43'
+    BLUE = '34'
+    BLUE_b = '44'
+    MAGENTA = '35'
+    MAGENTA_b = '45'
+    CYAN = '36'
     CYAN_b = '46'
+    WHITE = '37'
+    WHITE_b = '47'
+    
+    
    
 
     def __init__(self, color, back):
@@ -14,11 +27,22 @@ class Color:
     @staticmethod
     def int_to_color(input):
         int_to_color_dict = {
-            1: Color.RED,
-            2: Color.GREEN,
-            3: Color.YELLOW,
-            10: Color.WHITE_b,
-            11: Color.CYAN_b,
+            1: Color.BLACK,
+            10: Color.BLACK_b,
+            2: Color.RED,
+            20: Color.RED_b,
+            3: Color.GREEN,
+            30: Color.GREEN_b,
+            4: Color.YELLOW,
+            40: Color.YELLOW_b,
+            5: Color.BLUE,
+            50: Color.BLUE_b,
+            6: Color.MAGENTA,
+            60: Color.MAGENTA_b,
+            7: Color.CYAN,
+            70: Color.CYAN_b,
+            8: Color.WHITE,
+            80: Color.WHITE_b
         }
 
         return int_to_color_dict.get(input)
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from Task import Color
 def prn():
     user_text = input("Enter some text: ")
     try:
-        user_input = int(input("""Enter text color:\n 1: RED\n 2: GREEN\n 3: YELLOW\n Your choise: """))
-        back = int(input("""Enter background color:\n 11: CYAN\n 10: WHITE\n  Your choise: """))
+        user_input = int(input("""Enter text color:\n 1: BLACK\n 2: RED\n 3: GREEN\n 4: YELLOW
+ 5: BLUE\n 6: MAGENTA\n 7: CYAN\n 8: WHITE\n Your choise: """))
+        
+        back = int(input("""Enter background color:\n 10: BLACK\n 20: RED\n 30: GREEN\n 40: YELLOW\n
+ 50: BLUE\n 60: MAGENTA\n 70: CYAN\n 80: WHITE\n Your choise: """))
+    
     except ValueError as ex:
         print("This is not number, try again. ", ex)
         return
