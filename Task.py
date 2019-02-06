919fbf6cace240dfd0e9e7a1293dc18b74907030
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-06 14:06:57 +0300

    Fixed background color

--- Task.py
+++ Task.py
@@ -1,26 +1,30 @@
 class Color: 
-    RED = '0;31'
-    GREEN = '0;32'
-    YELLOW = '1;33'
+    RED = '31'
+    GREEN = '32'
+    YELLOW = '33'
+    WHITE_b = '47'
+    CYAN_b = '46'
    
 
-    def __init__(self, color):
+    def __init__(self, color, back):
         self.color = color 
+        self.back = back
         
-
+        
     @staticmethod
     def int_to_color(input):
         int_to_color_dict = {
-            '1': Color.RED,
-            '2': Color.GREEN,
-            '3': Color.YELLOW
+            1: Color.RED,
+            2: Color.GREEN,
+            3: Color.YELLOW,
+            10: Color.WHITE_b,
+            11: Color.CYAN_b,
         }
 
-        return int_to_color_dict.get(input, Color.RED)
-        
+        return int_to_color_dict.get(input)
 
     def __enter__(self):
-        print('\033[{}m'.format(self.color))
+        print('\033[1;{}m\033[1;{}m'.format(self.color, self.back))
 
 
     def __exit__(self, exc_type, exc_value, tb):
--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from Task import Color
 def prn():
     user_text = input("Enter some text: ")
     try:
-        user_input = int(input("""Enter color:\n 1: RED\n 2: GREEN\n 3: YELLOW\n Your choise: """))
+        user_input = int(input("""Enter text color:\n 1: RED\n 2: GREEN\n 3: YELLOW\n Your choise: """))
+        back = int(input("""Enter background color:\n 11: CYAN\n 10: WHITE\n  Your choise: """))
     except ValueError as ex:
         print("This is not number, try again. ", ex)
         return
 
     color = Color.int_to_color(user_input)
-    
-    with Color(color):
+    back = Color.int_to_color(back)
+
+    with Color(color, back):
         print(user_text)
 
 
