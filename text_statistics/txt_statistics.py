ab7dae037215f2646870d5c33e91d557e02d9c1e
PolinaAlexandr <polinaalexnder@gmail.com>
2019-03-20 14:47:47 +0300

    Added txt_statistics basic structure

---
+++ text_statistics/txt_statistics.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from os import path
+from collections import Counter
+
+class Text_stats:
+
+    def __init__(self, file_path, words_count):
+        self.file_path = file_path
+        self.words_count = words_count
+        
+
+root_dir = os.path.expanduser('~')
+txt_files = list() 
+
+for root, dirs, files in os.walk(root_dir, topdown=True):
+    if '.txt' in files:
+        texts = []
+        words_count = list(Counter(texts[0]).most_common(1)[0])
+
+
+
+
+if __name__ == "__main__":
+    pass
